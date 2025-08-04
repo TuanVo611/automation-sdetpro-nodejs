b117932580ae7d303b1a302598c2345e62502cf6
TuanVo <vo.anhtuan.0611@gmail.com>
2025-08-04 17:01:48 +0700

    Update Lab 04

--- labs/lab_4/Lab_4.js
+++ labs/lab_4/Lab_4.js
@@ -12,30 +12,31 @@ user2.accountNumber = "4221498696356999";
 user2.balance = 2000000
 
 let bankAccounts = [user1, user2];
+
 //Usage
 const readline = require("readline-sync")
-function showMenu(){
-let isStillUsing = true;
+function showMenu() {
+    let isStillUsing = true;
 
-while (isStillUsing) {
-    printBankMenuApplication();
-    let userOption = getUserOption();
+    while (isStillUsing) {
+        printBankMenuApplication();
+        let userOption = getUserOption();
 
-    if (userOption === 0) {
-        isStillUsing = false;
-    } else if (userOption === 1) {
-        findAnAccount(bankAccounts);
-    } else if (userOption === 2) {
-        updateBalance(bankAccounts);
-    } else {
-        console.log("Input Wrong Number! Input Again Please!")
+        if (userOption === 0) {
+            isStillUsing = false;
+        } else if (userOption === 1) {
+            findAnAccount(bankAccounts);
+        } else if (userOption === 2) {
+            updateBalance(bankAccounts);
+        } else {
+            console.log("Input Wrong Number! Input Again Please!")
+        }
     }
-}
 
-console.log(`See You Again!`)
+    console.log(`See You Again!`)
 
-function printBankMenuApplication() {
-    console.log(`
+    function printBankMenuApplication() {
+        console.log(`
     /**
      * === Banking application===
      * 1. Find an account
@@ -43,7 +44,7 @@ function printBankMenuApplication() {
      * 0. Exit the program
     */
     `)
-}
+    }
 }
 
 function getUserOption() {
@@ -53,16 +54,22 @@ function getUserOption() {
 //Controller
 function findAnAccount(bankAccounts) {
     let accountNumberInput = getInputAccountNumber();
+    let count = 0;
     for (const bankAccount of bankAccounts) {
         if (accountNumberInput === bankAccount.accountNumber) {
             console.log(`Name account bank is : ${bankAccount.name} with balance: ${bankAccount.balance})`);
         } else {
-            console.log(`Your account number is incorrect! Please input your account number again!`);
+            count++;
         }
-    } 
+    }
+    if (count === bankAccounts.length) {
+        console.log(`Your account number is incorrect! Please input your account number again!`);
+    }
+
 }
 
 function updateBalance(bankAccounts) {
+    let count = 0;
     let accountNumberInput = getInputAccountNumber();
     for (const bankAccount of bankAccounts) {
         if (accountNumberInput === bankAccount.accountNumber) {
@@ -73,12 +80,17 @@ function updateBalance(bankAccounts) {
                 re-enter the amount you want to withdraw.`);
                 updateBalance(bankAccounts);
             } else {
-                console.log(`Name account bank is : ${bankAccount.name} with remaining balance: ${balanceAccount - withDrawalAmount})`);  
+                console.log(`Name account bank is : ${bankAccount.name} with remaining balance: ${balanceAccount - withDrawalAmount})`);
             }
         } else {
-            console.log(`Your account number is incorrect! Please input your account number again!`);
+            count++;
         }
     }
+    
+    if (count === bankAccounts.length) {
+        console.log(`Your account number is incorrect! Please input your account number again!`);
+    }
+
 }
 
 //Support Function
