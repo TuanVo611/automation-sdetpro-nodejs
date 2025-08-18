0528af582cb7ddba039ad8d494eff40394daf137
TuanVo <vo.anhtuan.0611@gmail.com>
2025-08-18 20:59:55 +0700

    Lab 6

--- labs/lab_6/Lab_6_2.js
+++ labs/lab_6/Lab_6_2.js
@@ -1,5 +1,5 @@
 //Data
-const urlEndpoint = "https://jsonplaceholder.typicode.com/posts";
+const urlEndpoint = "https://jsonplaceholder.typicode.com/posts/";
 const readLine = require("readline-sync");
 let userId = Number(readLine.question("Please Input Your User Id: "));
 let postId = Number(readLine.question("Please Input Your Post Id: "));
@@ -7,49 +7,37 @@ let postId = Number(readLine.question("Please Input Your Post Id: "));
 //Usage
 getInforUser(urlEndpoint, userId, postId);
 
-//Logic - from controller
-function getInforUser(url, userId, postId) {
-    return fetch(url)
-        .then(function (response) {
-            return getResponse(response);
-        })
-        .then(function (validateRespone) {
-            const filterResponseData = filterData(validateRespone, userId, postId)
-            console.log(filterResponseData);
+//Logic - From Controller
+async function getInforUser(url, userId, postId) {
+    const response = await fetch(url);
+    const validateRespone = await response.json();
 
-            const allPostData = getAllPostsData(validateRespone, userId);
-            console.log(allPostData)
-        });
+    const filterResponseData = filterData(validateRespone, userId, postId)
+    console.log(filterResponseData);
 
+    const allPostData = getAllPostsData(validateRespone, userId);
+    console.log(allPostData)
 }
 
-//Support Functions
-function getResponse(response) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            const responseData = response.json();
-            resolve(responseData);
-        }, 3 * 1000);
-    })
-}
 
+//Support Function
 function filterData(validateRespone, userId, postId) {
-    console.log(`Content user with user id: ${userId} and post id: ${postId}`)
     let countUserId = 0;
+    console.log(`Content user with user id: ${userId} and post id: ${postId}`)
     for (let index = 0; index < validateRespone.length; index++) {
         if (Number(validateRespone[index].userId) === userId) {
             if (Number(validateRespone[index].id === postId)) {
                 console.log(`Post id: ${validateRespone[index].id}`)
-                id = validateRespone[index].id
                 console.log(`User id: ${validateRespone[index].userId}`)
-                resultUserId = validateRespone[index].userId
                 console.log(`Title: ${validateRespone[index].title}`)
-                title = validateRespone[index].title
                 console.log(`Body: ${validateRespone[index].body}`)
-                body = validateRespone[index].body
             }
         } else {
             countUserId++;
+        }
+
+        if (countUserId === validateRespone.length) {
+            console.log(`Does not exist user id: ${userId}`);
         }
     }
 
@@ -59,6 +47,7 @@ function filterData(validateRespone, userId, postId) {
 }
 
 function getAllPostsData(validateRespone, userId) {
+    let countUserId = 0;
     console.log(`Content all post for user with user id: ${userId}`)
     for (let index = 0; index < validateRespone.length; index++) {
         if (Number(validateRespone[index].userId) === userId) {
@@ -67,5 +56,8 @@ function getAllPostsData(validateRespone, userId) {
             countUserId++;
         }
     }
-}
 
+    if (countUserId === validateRespone.length) {
+        console.log(`Does not exist user id: ${userId}`);
+    }
+}
